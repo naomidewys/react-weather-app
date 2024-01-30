5d8856506acd3a0371c140935fd51a290eb47a34
naomidewys <146399253+naomidewys@users.noreply.github.com>
2024-01-30 14:25:29 +1000

    Added basic styling with CSS

--- src/Weather.js
+++ src/Weather.js
@@ -11,16 +11,27 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Brisbane</h1>
       <p>Monday 18:00</p>
-      <h2>⛅ 27°C</h2>
+      <h2>
+        ⛅{" "}
+        <span className="temperature">
+          <strong>27</strong>
+        </span>
+        <span className="unit">°C</span>
+      </h2>
       <p>
         Humidity: <strong>72%</strong> • Wind: <strong>13 km/h</strong>
         <br />
